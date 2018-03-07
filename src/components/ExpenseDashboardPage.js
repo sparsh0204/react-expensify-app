21ce53d5a9c2c036e7e546495b542da912440634
Sparsh Agarwal <sparshagarwal205@gmail.com>
2018-03-07 16:19:05 +0530

    Initial commit

---
+++ src/components/ExpenseDashboardPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ExpenseList from './ExpenseList';
+import ExpenseListFilters from './ExpenseListFilters';
+
+const ExpenseDashboardPage = () => (
+    <div>
+        <br/>
+        <ExpenseListFilters/>
+      <ExpenseList/>
+    </div>
+);
+
+export default ExpenseDashboardPage;
---
+++ src/selectors/expenses.js
@@ -0,0 +1,21 @@
+import moment from 'moment';
+
+// const getVisibleExpense =
+const getVisibleExpense = (expenses, {text, sortBy, startDate, endDate}) => {
+    return expenses.filter((expense) => {
+        const createdAtMoment = moment(expense.createdAt);
+        const startDateMatch = startDate ? startDate.isSameOrBefore(createdAtMoment, 'day') : true;  //typeof startDate !== 'number' || expense.createdAt >= startDate;
+        const endDateMatch = endDate ? endDate.isSameOrAfter(createdAtMoment, 'day') : true;  //typeof endDate !== 'number' || expense.createdAt <= endDate;
+        const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
+
+        return startDateMatch && endDateMatch && textMatch;
+    }).sort((a, b) => {
+        if(sortBy === 'date') {
+            return a.createdAt < b.createdAt ? 1 : -1;
+        }else if(sortBy === 'amount') {
+            return a.amount < b.amount ? 1 : -1;
+        }
+    });
+};
+
+export default getVisibleExpense;
