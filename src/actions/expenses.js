6850a0d555a93b15473d657732a448bd9a466685
Sparsh Agarwal <sparshagarwal205@gmail.com>
2018-03-10 17:38:25 +0530

    Database integration completed

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -32,12 +32,29 @@ export const removeExpense = ({id} = {}) => ({
     type: 'REMOVE_EXPENSE',
     id
 });
+
+export const startRemoveExpense = ({id} = {}) => {
+    return (dispatch) => {
+        return database.ref(`expenses/${id}`).remove().then(() => {
+            dispatch(removeExpense({id}));
+        });
+    }
+};
+
 // EDIT_EXPENSE
 export const editExpense = (id, updates) => ({
     type: 'EDIT_EXPENSE',
     id,
     updates
 });
+
+export const startEditExpense = (id, updates) => {
+    return (dispatch) => {
+        return database.ref(`expenses/${id}`).update(updates).then(() => {
+            dispatch(editExpense(id,updates));
+        });
+    }
+};
 
 export const setExpenses = (expenses) => ({
     type: 'SET_EXPENSES',
